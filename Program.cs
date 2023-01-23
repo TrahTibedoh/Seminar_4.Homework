25736323c41d9df0eb7dbd29ef54d5b7818c9336
Vadim Stepanov <vvs0703@mail.ru>
2023-01-23 06:32:54 +0300

    Seminar4_Homework

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿//Напишите программу которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+int Square(int a, int b )
+{
+    int result = 1;
+    for(int i = 1; i <= b; i++)
+        result *= a;
+    return result;
+}
+
+Console.Write("Input number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Square(a,b));
+*/
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
+int Sum(int number)
+{
+    int result = 0;
+    int current = 0;
+    while(number > 0)
+    {
+        current = number % 10;
+        result = result + current;
+        number = number / 10;
+        current++;
+    }
+    return result;
+}
+
+Console.Write("Input number: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Sum(numb));
+*/
+
+//Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+/*
+int[] CreateArray() 
+
+ {
+    Console.Write("Введите количество элементов масива:   ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[] myArray = new int[m];
+    for(int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"Введите элемент масива под индексом {i}:  ");
+        myArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return myArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.WriteLine(array[i]);
+
+    Console.WriteLine();    
+}
+int[] newArray = CreateArray();
+ShowArray(newArray);
+*/
